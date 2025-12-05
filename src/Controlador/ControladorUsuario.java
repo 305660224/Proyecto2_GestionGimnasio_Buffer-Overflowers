39a4c2a718b94f73a0fc8fd61550f3bc5cca3eac
Jeshuan29 <jetorresgo@est.utn.ac.cr>
2025-12-05 12:20:40 -0600

    ControladorUsuario
    Clase encargada de encapsular metodos CRUD necesarioas para gestionar los usuarios

---
+++ src/Controlador/ControladorUsuario.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+import java.util.List;
+import Modelo.Usuario;
+import Modelo.EnumRol;
+import Modelo.SesionUsuario;
+import Modelo.UsuarioDAO;
+import Utilidades.Seguridad;
+/**
+ *
+ * @author Jeshuan
+ */
+public class ControladorUsuario {
+    private UsuarioDAO usuarioDAO;
+    
+    public ControladorUsuario(){
+       this.usuarioDAO = usuarioDAO;
+    }
+    
+    /**
+     * Método para registrar un nuevo empleado en el sistema.
+     * Este método lo usará la ventana de "Administración de Usuarios".
+     */
+    public boolean agregarUsuario(String nombreUsuario, String passwordPlana, int idRol) {
+        if (nombreUsuario == null || nombreUsuario.isEmpty()) {
+            System.out.println("Error: El usuario no puede estar vacío");
+            return false;
+        }
+        
+        if (usuarioDAO.buscarPorUsername(nombreUsuario) != null) {
+            System.out.println("Error: El nombre de usuario ya existe");
+            return false;
+        }
+
+        Usuario nuevoUsuario = new Usuario();
+        nuevoUsuario.setUsuario(nombreUsuario);
+
+        byte[] passEncriptada = Seguridad.encriptar(passwordPlana);
+        nuevoUsuario.setContrasena(passEncriptada);
+
+        nuevoUsuario.setIdRol(EnumRol.porId(idRol));
+        return usuarioDAO.registrar(nuevoUsuario);
+    }
+    public boolean eliminarUsuario(int idUsuario) {
+        Usuario logueado = SesionUsuario.getInstancia().getUsuarioLogueado();
+        if (logueado != null && logueado.getIdusuario() == idUsuario) {
+            return false;
+        }   
+        return usuarioDAO.eliminar(idUsuario);
+    }   
+    
+    /**
+     * Método para listar todos los usuarios y mostrarlos en una tabla
+     */
+    public List<Usuario> obtenerTodosLosUsuarios() {
+        return usuarioDAO.listarTodos(); 
+    }
+}
